db68562ae801d59f2157358ef5da7d8386798f63
shexiaoheng <shexiaoheng@gmail.com>
2016-02-26 16:50:27 +0800

    init

---
+++ src/main/java/com/bee/customdialog/PopupWindowPackage.java
@@ -0,0 +1,32 @@
+package com.bee.customdialog;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by heng on 16/2/26.
+ */
+public class PopupWindowPackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Arrays.<NativeModule>asList(new PopupWindowModule(reactContext));
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
